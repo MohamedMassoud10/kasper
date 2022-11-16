f2b32ebc27dfee8cba564a04b00e02c7ab94ec50
MohamedMassoud10 <massoudfcai@gmail.com>
2022-11-16 17:19:21 +0200

    scrolling

--- src/component/About.js
+++ src/component/About.js
@@ -1,4 +1,8 @@
+import ScrollTrigger from "react-scroll-trigger";
+import CountUp from "react-countup";
+import React from "react"
 let About=()=>{
+  let [counterOn,setCounterOn]=React.useState(false);
     return(
         <div className="About" id="about">
             <div className="const-div">
@@ -6,31 +10,32 @@ let About=()=>{
                 <p>Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.<br/> Mauris blandit aliquet elit, eget tincidunt.</p>
                 <img src="../imges/about.png" alt="" />
             </div>
-            <div class="stats">
-              <div className="overlay"></div>
-      <div class="stats-cont">
-        <div class="box">
-          <i class="fas fa-mug-hot"></i>
-          <div class="number">1.236</div>
-          <p>Coffee Drinks</p>
+                <ScrollTrigger onEnter={()=>setCounterOn(true)} onExit={()=>setCounterOn(false)}>
+                <div class="stats">
+                  <div className="overlay"></div>
+          <div class="stats-cont">
+            <div class="box">
+              <i class="fas fa-mug-hot"></i>
+              {counterOn && <CountUp start={0} end={1250} duration={3} delay={0} className={"count"}/>}
+              <p>Coffee Drinks</p>
+            </div>
+            <div class="box">
+              <i class="far fa-folder"></i>
+              {counterOn &&<CountUp start={0} end={256} duration={3} delay={0} className={"count"}/>}  
+                          <p>Completed Projects</p>
+            </div>
+            <div class="box">
+              <i class="far fa-envelope"></i>
+              {counterOn &&<CountUp start={0} end={1743} duration={3} delay={0} className={"count"}/>}
+              <p>Mail Sent</p>
+            </div>
+            <div class="box">
+              <i class="fas fa-trophy"></i>
+              {counterOn &&<CountUp start={0} end={17} duration={3} delay={0} className={"count"}/>}             <p>Awards Received</p>
+            </div>
+          </div>
         </div>
-        <div class="box">
-          <i class="far fa-folder"></i>
-          <div class="number">256</div>
-          <p>Completed Projects</p>
-        </div>
-        <div class="box">
-          <i class="far fa-envelope"></i>
-          <div class="number">1,743</div>
-          <p>Mail Sent</p>
-        </div>
-        <div class="box">
-          <i class="fas fa-trophy"></i>
-          <div class="number">17</div>
-          <p>Awards Received</p>
-        </div>
-      </div>
-    </div>
+        </ScrollTrigger>
 
         </div>
     )
